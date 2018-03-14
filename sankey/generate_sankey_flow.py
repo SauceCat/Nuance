cbc5109acae3dfc7d58811b5c24dc7e6e74c4682
saucecat <jiangchun.lee@gmail.com>
2018-03-14 23:54:00 +0800

    test color_mapping

--- sankey/generate_sankey_flow.py
+++ sankey/generate_sankey_flow.py
@@ -1,7 +1,4 @@
 
-import pandas as pd
-import numpy as np
-
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -17,14 +14,14 @@ def _get_node_names(df):
 	node_infos = {}
 
 	for col in df.columns.values:
-	    col_values = sorted(df[col].unique())
-	    col_names = []
+		col_values = sorted(df[col].unique())
+		col_names = []
 
-	    for col_value in col_values:
-	        node_name = '%s: %s' %(col, str(col_value))
-	        col_names.append(node_name)
+		for col_value in col_values:
+			node_name = '%s: %s' %(col, str(col_value))
+			col_names.append(node_name)
 
-	    node_infos[col] = {'col_names': col_names, 'col_values': col_values}
+		node_infos[col] = {'col_names': col_names, 'col_values': col_values}
 
 	return node_infos
 
@@ -32,64 +29,65 @@ def _get_node_names(df):
 def _get_node_colors(node_infos, node_color_type, node_color_mapping, cm):
 	node_colors = {}
 
-	if node_color_type == 'col':
-	    for col_idx, col in enumerate(node_infos.keys()):
-	        col_color = cm(col_idx % 20)
-	        for col_name in node_infos[col]['col_names']:
-	            node_colors[col_name] = matplotlib.colors.rgb2hex(col_color)
-	            
-	if node_color_type == 'val':
-	    unique_values = []
-	    for col in node_infos.keys():
-	        unique_values += list(node_infos[col]['col_values'])
-	    unique_values = list(set(unique_values))
-	    for col in node_infos.keys():
-	        for col_value in node_infos[col]['col_values']:
-	            val_idx = unique_values.index(col_value)
-	            node_colors['%s: %s' %(col, str(col_value))] = matplotlib.colors.rgb2hex(cm(val_idx % 20))
-	            
-	if node_color_type == 'col_val':
-	    node_names = []
-	    for col in node_infos.keys():
-	        node_names += node_infos[col]['col_names']
-	    for node_name_idx, node_name in enumerate(node_names):
-	        node_colors[node_name] = matplotlib.colors.rgb2hex(cm(node_name_idx % 20))
-	        
-	if node_color_type == 'cus':
-	    for col in node_color_mapping.keys():
-	        if type(node_color_mapping[col]) == dict:
-	            for col_val in node_color_mapping[col].keys():
-	                node_colors['%s: %s' %(col, str(col_val))] = node_color_mapping[col][col_val]
-	        else:
-	            for col_name in node_infos[col]['col_names']:
-	                node_colors[col_name] = node_color_mapping[col]
+	if (node_color_type == 'col') or (node_color_type == 'cus' and node_color_mapping['type'] == 'col'):
+		for col_idx, col in enumerate(node_infos.keys()):
+			if node_color_type == 'cus':
+				col_color = node_color_mapping['mapping'][col]
+			else:
+				col_color = cm(col_idx % 20)
+			for col_name in node_infos[col]['col_names']:
+				node_colors[col_name] = matplotlib.colors.rgb2hex(col_color)
+
+	if (node_color_type == 'val') or (node_color_type == 'cus' and node_color_mapping['type'] == 'val'):
+		unique_values = []
+		for col in node_infos.keys():
+			unique_values += list(node_infos[col]['col_values'])
+		unique_values = list(set(unique_values))
+
+		for col in node_infos.keys():
+			for col_value in node_infos[col]['col_values']:
+				if node_color_type == 'cus':
+					val_color = node_color_mapping['mapping'][col_value]
+				else:
+					val_idx = unique_values.index(col_value)
+					val_color = matplotlib.colors.rgb2hex(cm(val_idx % 20))
+				node_colors['%s: %s' %(col, str(col_value))] = val_color
+
+	if (node_color_type == 'col_val') or (node_color_type == 'cus' and node_color_mapping['type'] == 'col_val'):
+		for col_idx, col in enumerate(node_infos.keys()):
+			if node_color_type == 'cus':
+				for col_value in node_color_mapping['mapping'][col]:
+					node_colors['%s: %s' % (col, str(col_value))] = node_color_mapping['mapping'][col][col_value]
+			else:
+				for col_name_idx, col_name in enumerate(node_infos[col]['col_names']):
+					col_name_idx_true = col_idx * len(node_infos[col]['col_names']) + col_name_idx
+					node_colors[col_name] = matplotlib.colors.rgb2hex(cm(col_name_idx_true % 20))
 
 	return node_colors
 
 
 def _prepare_sankey_data(df, node_colors):
 	links = []
-	nodes = []
 	for i in range(len(df.columns.values)-1):
-	    source_col, target_col = df.columns.values[i], df.columns.values[i+1]
-	    temp_df = df[[source_col, target_col]]
-	    temp_df['count'] = 1
-	    temp_df = temp_df.rename(columns={source_col: 'source', target_col: 'target'})
-	    temp_df_gp = temp_df.groupby(['source', 'target'], as_index=False).count()
-	    
-	    temp_df_gp['source'] = temp_df_gp['source'].apply(lambda x : '%s: %s' %(source_col, str(x)))
-	    temp_df_gp['target'] = temp_df_gp['target'].apply(lambda x : '%s: %s' %(target_col, str(x)))
-	    
-	    temp_df_gp['color_source'] = temp_df_gp['source'].apply(lambda x : node_colors[x] if x in node_colors.keys() else '#000')
-	    temp_df_gp['color_target'] = temp_df_gp['target'].apply(lambda x : node_colors[x] if x in node_colors.keys() else '#000')
-	    temp_df_gp['value'] = temp_df_gp['count'].map(str)
-	    
-	    links+= temp_df_gp[['source', 'target', 'value']].to_dict('records')
+		source_col, target_col = df.columns.values[i], df.columns.values[i+1]
+		temp_df = df[[source_col, target_col]]
+		temp_df['count'] = 1
+		temp_df = temp_df.rename(columns={source_col: 'source', target_col: 'target'})
+		temp_df_gp = temp_df.groupby(['source', 'target'], as_index=False).count()
+
+		temp_df_gp['source'] = temp_df_gp['source'].apply(lambda x : '%s: %s' %(source_col, str(x)))
+		temp_df_gp['target'] = temp_df_gp['target'].apply(lambda x : '%s: %s' %(target_col, str(x)))
+
+		temp_df_gp['color_source'] = temp_df_gp['source'].apply(lambda x : node_colors[x] if x in node_colors.keys() else '#000')
+		temp_df_gp['color_target'] = temp_df_gp['target'].apply(lambda x : node_colors[x] if x in node_colors.keys() else '#000')
+		temp_df_gp['value'] = temp_df_gp['count'].map(str)
+
+		links+= temp_df_gp[['source', 'target', 'value']].to_dict('records')
 
 	nodes = [{'name': n, 'color': c} for (n, c) in node_colors.items()]
 	data = {
-	    'links': links,
-	    'nodes': nodes
+		'links': links,
+		'nodes': nodes
 	}
 	return data
 
@@ -122,9 +120,9 @@ def draw_sankey_flow(df, node_color_type, link_color_type, width, height, graph_
 	else:
 		output_path = 'sankey_flow_output_%s.html' %(graph_name)
 	with open(output_path, 'wb') as fh:
-	    fh.write(template.render({'data': json.dumps(sankey_data), 'link_color_type': link_color_type, 'link_color': link_color, 
-	                              'width': width, 'height': height}))
-	    print('The output is in %s. Enjoy!' %(output_path))
+		fh.write(template.render({'data': json.dumps(sankey_data), 'link_color_type': link_color_type,
+								  'link_color': link_color, 'width': width, 'height': height}))
+	print('The output is in %s. Enjoy!' %(output_path))
 
 
 
