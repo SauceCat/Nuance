629ecdefed04277a58b190986f53a484621439b5
SauceCat <jiangchun.lee@gmail.com>
2018-03-18 13:30:24 +0800

    tidy up sankey flow

--- sankey/src/generate_sankey_flow.py
+++ sankey/src/generate_sankey_flow.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 import json
 import jinja2
+import os
 
 import sys
 reload(sys)
@@ -11,6 +12,13 @@ sys.setdefaultencoding('utf-8')
 
 
 def _get_node_names(df):
+	'''
+	:param df: pandas DataFrame, each column represents a state
+	:return:
+		dictionary of (column name, column unique value list)
+		which means (state name, unique values in this state)
+	'''
+
 	node_infos = {}
 
 	for col in df.columns.values:
@@ -27,6 +35,22 @@ def _get_node_names(df):
 
 
 def _get_node_colors(node_infos, node_color_type, node_color_mapping, cm):
+	'''
+	:param node_infos:
+		dictionary of (state name, unique values in this state)
+	:param node_color_type:
+		node coloring strategy, can be one of ['col', 'val', 'col_val', 'cus']
+		- 'col': each column has different color
+		- 'val': each unique value has different color (unique values through all columns)
+		- 'col_val': each unique value in each column has different color
+		- 'cus': customer provide node color mapping
+	:param node_color_mapping:
+		if node_color_type == 'cus', node_color_mapping should't be None
+	:param cm: matplotlib color map
+	:return:
+		dictionary of (node name, node color)
+	'''
+
 	node_colors = {}
 
 	if (node_color_type == 'col') or (node_color_type == 'cus' and node_color_mapping['type'] == 'col'):
@@ -67,6 +91,13 @@ def _get_node_colors(node_infos, node_color_type, node_color_mapping, cm):
 
 
 def _prepare_sankey_data(df, node_colors):
+	'''
+	:param df: pandas DataFrame, each column represents a state
+	:param node_colors: dictionary of (node name, node color)
+	:return:
+		dictionary of links and nodes
+	'''
+
 	links = []
 	for i in range(len(df.columns.values)-1):
 		source_col, target_col = df.columns.values[i], df.columns.values[i+1]
@@ -92,7 +123,41 @@ def _prepare_sankey_data(df, node_colors):
 	return data
 
 
-def draw_sankey_flow(df, node_color_type, link_color_type, width, height, graph_name=None, node_color_mapping=None, color_map=None, link_color=None):
+def draw_sankey_flow(df, node_color_type, link_color_type, width, height, graph_name=None,
+					 node_color_mapping=None, color_map=None, link_color=None):
+	'''
+	:param df:
+		pandas DataFrame, each column represents a state
+	:param node_color_type:
+		node coloring strategy, can be one of ['col', 'val', 'col_val', 'cus']
+			- 'col': each column has different color
+			- 'val': each unique value has different color (unique values through all columns)
+			- 'col_val': each unique value in each column has different color
+			- 'cus': customer provide node color mapping
+	:param link_color_type:
+		link coloring strategy, default='source'
+		Can be one of ['source', 'target', 'both', 'same']
+			- 'source': same color as the source node
+			- 'target': same color as the target node
+			- 'both': color from both target and source
+			- 'same': all links have same color
+	:param width: wdith
+	:param height: height
+	:param graph_name: name of the graph
+	:param node_color_mapping:
+		if node_color_type == 'cus', color_mapping should be provided
+		example:
+		node_color_mapping = {
+			'type': 'col',
+			'mapping': {
+				column1: color1, column2: color2, ...
+			}
+		}
+	:param color_map: matplotlib color map
+	:param link_color:
+		if link_color_type == 'same', link color should be provided
+	'''
+
 	# get node infos
 	node_infos = _get_node_names(df)
 
@@ -111,27 +176,19 @@ def draw_sankey_flow(df, node_color_type, link_color_type, width, height, graph_
 	sankey_data = _prepare_sankey_data(df, node_colors)
 
 	# render the output
-	temp = open('sankey_flow_template.html').read()
+	temp = open('src/sankey_flow_template.html').read()
 	template = jinja2.Template(temp)
+
+	# create the output root if it is not exits
+	if not os.path.exists('sankey_flow_output'):
+		os.mkdir('sankey_flow_output')
 
 	# generate output html
 	if graph_name is None:
-		output_path = 'sankey_flow_output.html'
+		output_path = 'sankey_flow_output/sankey_flow_output.html'
 	else:
-		output_path = 'sankey_flow_output_%s.html' %(graph_name)
+		output_path = 'sankey_flow_output/sankey_flow_%s.html' %(graph_name)
 	with open(output_path, 'wb') as fh:
 		fh.write(template.render({'data': json.dumps(sankey_data), 'link_color_type': link_color_type,
 								  'link_color': link_color, 'width': width, 'height': height}))
 	print('The output is in %s. Enjoy!' %(output_path))
-
-
-
-
-
-
-
-
-
-
-
-
